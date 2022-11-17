9e8210386a07d326dd9c22d02dc32faaa2d0e318
groupguanfang <1203970284@qq.com>
2022-11-17 21:57:41 +0800

    6

---
+++ README.md
@@ -0,0 +1,11 @@
+# 宝塔面板 Node.JS API SERVE 📦
+
+除了官方已公布的API之外，我本人也去爬了宝塔面板其他的API。
+
+## 注意 ✨
+* 支持CommonJS调用
+* 使用commonJS而非TS，后续会重构为TS
+* 可以直接克隆仓库之后运行`npm run dev`启动API项目
+
+## License 🚩
+MIT.
--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
-require("dotenv").config()
+require('dotenv').config()
 const port = process.env.WEB_PORT
 
-app.use('/system',require("./src/system"))
+app.use('/system', require('./src/system'))
 
 app.listen(port, () => {
   console.log(`BT app listening on port ${port}`)
